5f21c2fda0fd728e3e7f215fa330917a606a2ae2
Gladysgong <531404376@qq.com>
2019-03-03 21:52:25 +0800

    picEval post and views

--- picEval/views.py
+++ picEval/views.py
@@ -20,25 +20,57 @@ def post1(request):
             'error': None,
             'data': None
         }
-        port_testocrip = request.POST.get('port_testocrip')
-        port_baseocrip = request.POST.get('port_baseocrip')
-        port_testimgip = request.POST.get('port_testimgip')
-        port_baseimgip = request.POST.get('port_baseimgip')
-        port_langs = request.POST.get('port_langs')
-        port_tag = request.POST.get('port_tag')
         env_type = request.POST.get('env_type')
+        if env_type == '2':
 
-        user = UserInfo.objects.get(username='gongyanli')
-        resp = ImageTaskInfo.objects.create(username=user, env_type=env_type, langs=port_langs,
-                                            test_ocrip=port_testocrip,
-                                            base_ocrip=port_baseocrip, test_imgip=port_testimgip,
-                                            base_imgip=port_baseimgip, testtag=port_tag)
-        ImageTaskInfo_id = resp.id
-        langs = port_langs.split('_')
-        from_langs = langs[0]
-        print(from_langs)
-        to_langs = langs[1]
-        post_ocr(ImageTaskInfo_id, port_testocrip, port_baseocrip, port_testimgip, port_baseimgip, from_langs, to_langs)
+            port_testocrip = request.POST.get('port_testocrip')
+            port_baseocrip = request.POST.get('port_baseocrip')
+            port_testimgip = request.POST.get('port_testimgip')
+            port_baseimgip = request.POST.get('port_baseimgip')
+            port_langs = request.POST.get('port_langs')
+            port_tag = request.POST.get('port_tag')
+
+            user = UserInfo.objects.get(username='gongyanli')
+            resp = ImageTaskInfo.objects.create(username=user, env_type=env_type, langs=port_langs,
+                                                test_ocrip=port_testocrip,
+                                                base_ocrip=port_baseocrip, test_imgip=port_testimgip,
+                                                base_imgip=port_baseimgip, testtag=port_tag)
+            ImageTaskInfo_id = resp.id
+            langs = port_langs.split('_')
+            from_langs = langs[0]
+            print(from_langs)
+            to_langs = langs[1]
+            post_ocr(ImageTaskInfo_id, port_testocrip, port_baseocrip, port_testimgip, port_baseimgip, from_langs,
+                     to_langs)
+        elif env_type == '1':
+            deploy_ip = request.POST.get('deploy_ip')
+            deploy_path = request.POST.get('deploy_path')
+            deploy_username = request.POST.get('deploy_username')
+            deploy_password = request.POST.get('deploy_password')
+            source_ip = request.POST.get('source_ip')
+            source_path = request.POST.get('source_path')
+            source_user = request.POST.get('source_user')
+            source_pwd = request.POST.get('source_pwd')
+            deploy_check = request.POST.get('deploy_check')
+            deploy_tag = request.POST.get('deploy_tag')
+
+            user = UserInfo.objects.get(username='gongyanli')
+            resp = ImageTaskInfo.objects.create(username=user, env_type=env_type, langs=deploy_check,
+                                                svIp=deploy_ip, svPath=deploy_path,
+                                                svUser=deploy_username, svPass=deploy_password,
+                                                sourceIP=source_ip, sourcePath=source_path,
+                                                sourceUser=source_user, sourcePass=source_pwd,
+                                                testtag=deploy_tag)
+
+            ImageTaskInfo_id = resp.id
+            langs = port_langs.split('_')
+            from_langs = langs[0]
+            print(from_langs)
+            to_langs = langs[1]
+            post_ocr(ImageTaskInfo_id, port_testocrip, port_baseocrip, port_testimgip, port_baseimgip, from_langs,
+                     to_langs)
+        else:
+            print('未知评测类型！')
 
         return render(request, 'picEval/post.html')
 
