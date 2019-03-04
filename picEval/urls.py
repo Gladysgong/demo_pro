3e53257ec54e7eea8071089c38e6000c3a846eeb
Gladysgong <531404376@qq.com>
2019-03-04 17:25:45 +0800

    add id

--- picEval/urls.py
+++ picEval/urls.py
@@ -8,7 +8,7 @@ app_name = 'picEval'
 urlpatterns = [
     # re_path(r'^debug/$', views.post),
     re_path(r'^pic/$', views.post1),
-    re_path(r'^pic/detail$', views.detail),
+    re_path(r'^pic/detail_(?P<task_id>\d+).html$', views.detail),
     # re_path(r'^debug/del', views.debug_del),
     # re_path(r'^debug/diff$', views.debug_diff),
 
