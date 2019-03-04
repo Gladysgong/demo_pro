1c547dccb92c119805b93c731edfd33daa64d264
Gladysgong <531404376@qq.com>
2019-03-04 15:05:20 +0800

    setting

--- demo_pro/settings.py
+++ demo_pro/settings.py
@@ -78,9 +78,16 @@ WSGI_APPLICATION = 'demo_pro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'demo_pro',
+        'USER': 'root',
+        'PASSWORD': 'Websearch@qa66',
+        'HOST': '10.144.120.30',
+        'PORT': '3306',
+        'OPTIONS': {
+            'autocommit': True,
+        },
+    },
 }
 
 
