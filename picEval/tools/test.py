d149b13bf3e60ebf9838a2b0c377cfbd26058f1e
Gladysgong <531404376@qq.com>
2019-03-08 20:23:25 +0800

    change db

--- picEval/tools/test.py
+++ picEval/tools/test.py
@@ -1,8 +1,23 @@
 import requests
 import base64
-import os, json
-from picEval.models import ImageTaskInfo, ResultInfo
+import os, json, sys,time
+import pymysql
 
+# from picEval.models import ImageTaskInfo, ResultInfo
+# from picEval.tools.conf import *
+
+# 数据库配置
+database_host = "10.134.120.30"
+database_db = "demo_pro"
+database_image = "picEval_imagetaskinfo"
+database_result = "picEval_resultinfo"
+database_user = "root"
+database_pass = "Websearch@qa66"
+
+# 图片路径配置
+rootpath = r'/Users/apple/AnacondaProjects/demo_pro'
+origin_secpath = r'/static/origin/'
+dest_secpath = r'/static/dest/port/'
 
 # ocr接口：http://10.143.52.35:10098/v4/ocr/json
 # 参数：lang，image=base64串
@@ -20,6 +35,23 @@ from picEval.models import ImageTaskInfo, ResultInfo
 # 翻译回帖接口：http://api.image.sogou/v1/open/ocr_translate.json
 # 参数：from、to、image、result_type=image
 
+ImageTaskInfo_id = int(sys.argv[1])
+port_testocrip = sys.argv[2]
+port_baseocrip = sys.argv[3]
+port_testimgip = sys.argv[4]
+port_baseimgip = sys.argv[5]
+from_langs = sys.argv[6]
+to_langs = sys.argv[7]
+
+db = pymysql.connect(database_host, database_user, database_pass, database_db)
+cursor = db.cursor()
+
+
+def get_now_time():
+    timeArray = time.localtime()
+    return time.strftime("%Y-%m-%d %H:%M:%S", timeArray)
+
+
 def imageTobase64(path):
     with open(path, 'rb') as f:
         image = base64.b64encode(f.read())
@@ -27,23 +59,29 @@ def imageTobase64(path):
         return image
 
 
-def post_ocr(ImageTaskInfo_id, test_ocrip, base_ocrip, test_imgip, base_imgip, from_langs, to_langs):
+def post_ocr(mission_id, test_ocrip, base_ocrip, test_imgip, base_imgip, from_langs, to_langs):
     headers = {
         'Content-Type': "application/x-www-form-urlencoded",
     }
 
-    origin_rootpath = r'/Users/apple/AnacondaProjects/demo_pro'
-    origin_secpath = r'/static/origin/'
-
-    sum_num = len(os.listdir(origin_rootpath + origin_secpath))
+    origin_path = rootpath + origin_secpath + from_langs + '_' + to_langs + '/'
+    ori_stroePaht = origin_secpath + from_langs + '_' + to_langs + '/'
+    sum_num = len(os.listdir(origin_path))
 
     failed = 0
     finished = 0
+    img_diff_count = 0
+    text_diff_count = 0
+    text_base_count = 0
+    print('sum',sum_num)
 
-    ImageTaskInfo.objects.filter(id=ImageTaskInfo_id).update(sum_num=sum_num)
+    sql_sum = "UPDATE %s set sum_num='%d', status=8 where id=%d" % (database_image, sum_num, mission_id)
+    cursor.execute(sql_sum)
+    db.commit()
+    # ImageTaskInfo.objects.filter(id=ImageTaskInfo_id).update(sum_num=sum_num)
 
-    for filename in os.listdir(origin_rootpath + origin_secpath):
-        base64image = imageTobase64(origin_rootpath + origin_secpath + filename)
+    for filename in os.listdir(origin_path):
+        base64image = imageTobase64(origin_path + filename)
         params_ocr = {
             'lang': from_langs,
             'image': base64image,
@@ -59,38 +97,52 @@ def post_ocr(ImageTaskInfo_id, test_ocrip, base_ocrip, test_imgip, base_imgip, f
 
         if (test_issuccess == int(1) & base_issuccess == int(1)):
             finished += 1
-            ImageTaskInfo.objects.filter(id=ImageTaskInfo_id).update(finished=finished)
-            distance()
+
+            distance_data = distance(ocr_test, ocr_base)
+
+            img_diff_count += distance_data['img_diff_count']
+            text_diff_count += distance_data['text_diff_count']
+            text_base_count += distance_data['text_base_count']
+
+            rankInfo = distance_data['sum_distance']
+            result = json.dumps(distance_data['result'])
+
+            # resp = ResultInfo.objects.create(taskid_id=int(ImageTaskInfo_id), testImg=ori_stroePaht + filename,
+            #                                  rankInfo=rankInfo, result=result, filename=filename,
+            #                                  test_status=test_issuccess, base_status=base_issuccess)
+            # ResultInfo_id = resp.id
+
+            test_Img1, testpath = post_image(from_langs, to_langs, base64image, test_imgip, filename, 'test')
+            test_Img2, basepath = post_image(from_langs, to_langs, base64image, base_imgip, filename, 'base')
+
+            sql_result = "INSERT INTO  %s(taskid_id,rankInfo,result,testImg,basepath,testpath,test_status,base_status,filename) values('%d','%d','%s','%s','%s','%s','%d','%d','%s')" % (
+                database_result, mission_id, rankInfo, pymysql.escape_string(result), test_Img1, basepath,
+                testpath, test_issuccess, base_issuccess, filename)
+
+            cursor.execute(sql_result)
+            db.commit()
 
         else:
             failed += 1
-            ImageTaskInfo.objects.filter(id=ImageTaskInfo_id).update(failed=failed)
+            # ImageTaskInfo.objects.filter(id=ImageTaskInfo_id).update(failed=failed)
 
-        resp = ResultInfo.objects.create(taskid_id=int(ImageTaskInfo_id), testImg=origin_secpath + filename,
-                                         test_status=test_issuccess, base_status=base_issuccess)
-        ResultInfo_id = resp.id
+    # ImageTaskInfo.objects.filter(id=ImageTaskInfo_id).update(finished=finished, img_diff_count=img_diff_count,
+    #                                                          text_diff_count=text_diff_count,
+    #                                                          text_base_count=text_base_count,
+    #                                                          status=8)
 
-        post_image(ResultInfo_id, from_langs, to_langs, base64image, test_imgip, filename, 'test')
-        post_image(ResultInfo_id, from_langs, to_langs, base64image, base_imgip, filename, 'base')
+    sql_image = "UPDATE %s set end_time='%s', sum_num='%d',finished='%d',failed = '%d',img_diff_count='%d',text_diff_count = '%d',text_base_count = '%d',status=9 where id=%d" % (
+        database_image, get_now_time(), sum_num, finished, failed, img_diff_count, text_diff_count,
+        text_base_count,
+        mission_id)
 
-        distance(ocr_test, ocr_base)
-
-        # y = x['result']
-        # for i in y:
-        #     for k, v in i.items():
-        #         print('k',k)
-        #         print('v',v)
-
-        # y = xx['result']['content']
-        # print(y)
-        # z=json.loads(y.join())
-        # print(z['content'])
-        # # print(json.dumps(x['result']))
+    cursor.execute(sql_image)
+    db.commit()
 
     return sum_num
 
 
-def post_image(ResultInfo_id, from_langs, to_langs, base64image, url, filename, type):
+def post_image(from_langs, to_langs, base64image, url, filename, type):
     # module_path = os.path.dirname(__file__)
     # print(module_path)
     # pic_path = r'/Users/apple/AnacondaProjects/demo_pro/image/'
@@ -112,38 +164,67 @@ def post_image(ResultInfo_id, from_langs, to_langs, base64image, url, filename, 
     result = resp.json()
     pic = result['pic']
     pic = base64.b64decode(pic)
-    root_path = '/Users/apple/AnacondaProjects/demo_pro'
-    sec_path = '/static/dest/'
+    path = ''
 
-    if not os.path.exists(root_path + sec_path):
-        os.makedirs(root_path + sec_path)
+    isPath = rootpath + dest_secpath + from_langs + '_' + to_langs + '/' + filename + '/'
+    storePath = dest_secpath + from_langs + '_' + to_langs + '/' + filename + '/'
+
+    if not os.path.exists(isPath):
+        os.makedirs(isPath)
 
     if result['success'] == int(1):
+
+        testImg = origin_secpath + from_langs + '_' + to_langs + '/' + filename
         filename = filename[:-4]
+
         if type == 'test':
-            file = open(root_path + sec_path + filename + '_test.jpg', 'wb')
-            path = sec_path + filename + '_test.jpg'
+            file = open(isPath + filename + '_test.jpg', 'wb')
+            path = storePath + filename + '_test.jpg'
             file.write(pic)
-            ResultInfo.objects.filter(id=ResultInfo_id).update(testpath=path)
+            # ResultInfo.objects.filter(id=ResultInfo_id).update(testpath=path)
             file.close()
         elif type == 'base':
-            file = open(root_path + sec_path + filename + '_base.jpg', 'wb')
-            path = sec_path + filename + '_base.jpg'
+            file = open(isPath + filename + '_base.jpg', 'wb')
+            path = storePath + filename + '_base.jpg'
             file.write(pic)
-            ResultInfo.objects.filter(id=ResultInfo_id).update(basepath=path)
+            # ResultInfo.objects.filter(id=ResultInfo_id).update(basepath=path)
             file.close()
         else:
             print('回帖图请求类型未知！')
-    return 0
+        return testImg, path
+    else:
+        return 0
 
 
 def distance(result_test, result_base):
-    x = 1
-    y = 2
-    return x, y
+    json = {
+        'img_diff_count': 1,
+        'text_diff_count': 4,
+        'text_base_count': 5,
+        'sum_distance': 1,
+        'rankInfo': 2,
+        'result': [
+            {
+                "basecontent": "aaa",
+                "testcontent": "bbb",
+                "distance": "ccc",
+            }, {
+                "basecontent": "ddd",
+                "testcontent": "eee",
+                "distance": "fff",
+            }
+        ]
+    }
+
+    return json
+
+
+def a():
+    print('test!')
 
 
 if __name__ == '__main__':
     # post_ocr('http://api.image.sogou/v1/ocr/basic.json', 'http://api.image.sogou/v1/ocr/basic.json', 'zh-CHS')
     # post_image('en', 'zh-CHS', base64, filename, url, type)
-    pass
+    post_ocr(ImageTaskInfo_id, port_testocrip, port_baseocrip, port_testimgip, port_baseimgip, from_langs, to_langs)
+    # a()
