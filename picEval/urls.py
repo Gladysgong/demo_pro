cc84b23b8fb17897c2310d84c7c2b9aefe8bd1e7
Gladysgong <531404376@qq.com>
2019-03-19 17:00:57 +0800

    fix url

--- picEval/urls.py
+++ picEval/urls.py
@@ -8,7 +8,7 @@ app_name = 'picEval'
 urlpatterns = [
     # re_path(r'^debug/$', views.post),
     re_path(r'^pic/$', views.post1),
-    re_path(r'^pic/cancel).html$', views.cancel),
+    re_path(r'^pic/cancel', views.cancel),
     re_path(r'^pic/detail_(?P<task_id>\d+).html$', views.detail),
     re_path(r'^pic/log_(?P<task_id>\d+).html$', views.log),
     # re_path(r'^debug/del', views.debug_del),
