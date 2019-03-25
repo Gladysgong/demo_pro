987ffa94a7e4451a8da09a0c58a52e70a6568cf3
Gladysgong <531404376@qq.com>
2019-03-25 18:01:59 +0800

    add order

--- picEval/views.py
+++ picEval/views.py
@@ -137,7 +137,7 @@ def detail(request):
         else:
             rowRate=round(float(image.text_diff_count/image.text_base_count),2)
 
-        result = ResultInfo.objects.filter(taskid_id=task_id).order_by('rankInfo')
+        result = ResultInfo.objects.filter(taskid_id=task_id).order_by('-test_status')
         data['reslut_lst'] = json.loads(serializers.serialize("json", result))
         # print('data',data)
 
